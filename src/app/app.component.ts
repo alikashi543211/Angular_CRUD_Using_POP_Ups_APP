20729d93a55c9eda9b7e8a8c3e978aa62905b321
kashifalidevstudio <kashifali@devstudio.us>
2023-06-12 17:59:11 +0500

    verifying ngx toastr installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular_CRUD_Using_POP_Ups_APP';
+export class AppComponent implements OnInit{
+    title = 'Angular_CRUD_Using_POP_Ups_APP';
+    constructor(private _toastr: ToastrService) {
+
+    }
+    ngOnInit() {
+        this._toastr.success("Success", "Registration");
+
+    }
 }
